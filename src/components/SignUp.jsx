2eab1386d3e42c5ecc6f2eab936dff61564782ae
GuruF <guruprasath@aidaptive.com>
2023-08-12 21:14:01 +0530

    Sign Up Page Error Handling

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import { signUp } from "../api/api";
+import { Toast } from "primereact/toast";
 
 export default function SignUp() {
     const [user, setUser] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const navigate = useNavigate();
+    const toast = useRef(null);
 
     const handleEmailChange = (event) => {
         setUser(event.target.value);
@@ -15,22 +18,56 @@ export default function SignUp() {
         setPassword(event.target.value);
     };
 
-    const handleSubmit = (event) => {
+    const showSuccessToast = (message) => {
+        toast.current.show({ severity: "success", summary: "Success", detail: message, life: 3000 });
+    };
+
+    const showErrorToast = (message) => {
+        toast.current.show({ severity: "error", summary: "Error", detail: message, life: 5000 });
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         // Validate userName and password
         if (!user || !password) {
             setError("Please fill in both userName and password.");
             return;
         }
-        // Perform sign-up logic here
-        // For this example, we'll just navigate to the home page ("/")
-        navigate("/");
+        setError("")
+        try {
+            // Perform sign-up logic
+            const response = await signUp({ username: user, password });
+            console.log(response)
+            if (response.message === "User created successfully") {
+                // Handle successful sign-up
+                showSuccessToast("Sign up successful!");
+               setTimeout(()=>{
+                navigate("/login")
+               },1000)
+            } else {
+                // Handle sign-up error
+                setError("Sign up failed. Please try again.");
+                showErrorToast("Sign up failed. Please try again.");
+            }
+        } catch (error) {
+            if (error.response?.data?.message === "Username already exists") {
+                showErrorToast("User Already Exists. Please Go to Login Page");
+                setTimeout(()=>{
+                    navigate("/login")
+                   },3000)
+            } else {
+                setError("Sign up failed. Please try again.");
+                showErrorToast("Sign up failed. Please try again.");
+            }
+
+
+        }
     };
 
     return (
         <>
             <div className="flex min-h-screen flex-col items-center justify-center px-6 py-12 lg:px-8 bg-gray-100">
-                <div className="border-2 border-gray-300 py-10 w-1/3 bg-white rounded-md">
+                <div className="border-2 border-gray-300 py-10 w-2/3 bg-white rounded-md">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                         <img
                             className="mx-auto h-14 w-auto"
@@ -49,7 +86,7 @@ export default function SignUp() {
                                     htmlFor="userName"
                                     className="block text-sm font-medium leading-6 text-gray-900"
                                 >
-                                    User Name 
+                                    User Name
                                 </label>
                                 <div className="mt-2">
                                     <input
@@ -92,22 +129,20 @@ export default function SignUp() {
                                     Sign Up
                                 </button>
                                 <button
-                                onClick={()=>navigate("/login")}
+                                    onClick={() => navigate("/login")}
                                     className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 border-2 shadow-sm hover:bg-indigo-600 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
-
-                                  Go to Sign-In Page
+                                    Go to Sign-In Page
                                 </button>
                             </div>
                         </form>
                         <p className="mt-2 text-center text-red-600">{error}</p>
 
-                        <p className="mt-10 text-center text-sm text-gray-500">
-                            Developed by GuruF
-                        </p>
+                        <p className="mt-10 text-center text-sm text-gray-500">Developed by GuruF</p>
                     </div>
                 </div>
             </div>
+            <Toast ref={toast} />
         </>
     );
 }
